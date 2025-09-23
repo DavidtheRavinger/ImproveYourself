538c201d1316350bf134a6bdbecfead3e597fd2b
DavidtheRavinger <davidfersing90gmail.com>
2025-09-23 14:32:36 +0200

    new

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import express from "express";
+import dotenv from "dotenv";
+
 import { runAgent1 } from "./agents/agent1.js";
 import { runAgent2 } from "./agents/agent2.js";
 import { runAgent3 } from "./agents/agent3.js";
@@ -5,13 +8,40 @@ import { runAgent4 } from "./agents/agent4.js";
 import { runAgent5 } from "./agents/agent5.js";
 import { runAgent6 } from "./agents/agent6.js";
 
-async function main() {
-  console.log(await runAgent1("Hallo Agent1"));
-  console.log(await runAgent2("Hallo Agent2"));
-  console.log(await runAgent3("Hallo Agent3"));
-  console.log(await runAgent4("Hallo Agent4"));
-  console.log(await runAgent5("Hallo Agent5"));
-  console.log(await runAgent6("Hallo Agent6"));
-}
+dotenv.config();
 
-main();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hallo! Willkommen bei ImproveYourself"));
+
+// Routen für alle Agenten
+app.get("/agent1", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent1!";
+  res.send(await runAgent1(prompt));
+});
+app.get("/agent2", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent2!";
+  res.send(await runAgent2(prompt));
+});
+app.get("/agent3", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent3!";
+  res.send(await runAgent3(prompt));
+});
+app.get("/agent4", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent4!";
+  res.send(await runAgent4(prompt));
+});
+app.get("/agent5", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent5!";
+  res.send(await runAgent5(prompt));
+});
+app.get("/agent6", async (req, res) => {
+  const prompt = req.query.prompt || "Hallo Agent6!";
+  res.send(await runAgent6(prompt));
+});
+
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+// Beispielaufruf: http://localhost:3000/agent1?prompt=Wie%20ist%20das%20Wetter%20heute?
